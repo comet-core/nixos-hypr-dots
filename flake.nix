4407d083f41a24e04789cccbdde44825e0de1066
Subhro <de.subhrodeep@gmail.com>
2026-09-09 08:54:25 +0530

    Remove duplicate hardware configuration import

--- flake.nix
+++ flake.nix
@@ -34,7 +34,6 @@
         
         modules = [
           # System-level configurations
-          ./hardware-configuration.nix
           ./configuration.nix
           lanzaboote.nixosModules.lanzaboote
           
